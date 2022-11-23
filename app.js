346bc9a3ee0c6cea599a31f8fab2ff6362da1ac0
elhamalizadeh <alizade4591@gmail.com>
2022-11-23 13:53:18 +0330

    commit 2

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ let gamePlay = false;
 button.addEventListener("click",function(){
     if (!gamePlay){
         gamePlay = true;
-        //maker();
-        alert("hello");
+        maker();
         button.innerHTML="check combo";
     }
     else{
@@ -15,8 +14,17 @@ button.addEventListener("click",function(){
 });
 
 function maker(){
+    for (var x = 0; x < 6; x++){
     let el = document.createElement("input");
     el.setAttribute("type","number");
-    
-
+    el.max = 9;
+    el.min = 0;
+    el.style.width = "50px";
+    gameArea.appendChild(el);
+    el.value= 0;
+    el.size = 1;
+    el.classList.add("numb");
+    el.order = x;
+    }
 }
+
